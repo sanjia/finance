dda40afe39ac6b541b7a98abe1badd3ca1f6bdc7
7481875@qq.com <7481875@qq.com>
2016-11-29 16:29:51 +0800

    增加goquery和mahonia代码

---
+++ main/StatStockFundFlow.go
@@ -0,0 +1,28 @@
+package main
+
+import _ "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"sanjia/finance/stock"
+	"sanjia/finance/util"
+)
+
+func main() {
+	cfg := new(util.Config)
+	cfg.InitConfig("main/app.conf")
+	connectURI := cfg.Read("mysql", "connectURI")
+	var db *sql.DB
+	db, _ = sql.Open("mysql", connectURI)
+	defer db.Close()
+
+	arg_num := len(os.Args)
+	if arg_num != 2 {
+		fmt.Errorf("demo:./StatStockFundFlow 600000")
+		return
+	}
+
+	//保存股票资金流向数据
+	stock.SaveStockFundFlow(db, os.Args[1])
+}
--- stock/saveStockFundFlow.go
+++ stock/saveStockFundFlow.go
@@ -1,30 +1,31 @@
 package stock
+
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/sanjia/mahonia"
 	"database/sql"
 	"fmt"
-	"strings"
-	"strconv"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/sanjia/goquery"
+	"github.com/sanjia/mahonia"
+	"strconv"
+	"strings"
 )
 
 /*
 	保存股票资金流向数据
 */
-func SaveStockFundFlow(db *sql.DB,stockCode string)  {
-	url := "http://data.eastmoney.com/zjlx/" + stockCode + ".html";
+func SaveStockFundFlow(db *sql.DB, stockCode string) {
+	url := "http://data.eastmoney.com/zjlx/" + stockCode + ".html"
 	doc, _ := goquery.NewDocument(url)
 	doc.Find("table.tab1").Find("tbody>tr").Each(func(i int, s1 *goquery.Selection) {
 		var data []string
-		data = append(data, "601688")
+		data = append(data, stockCode)
 		s1.Find("td").Each(func(i int, s *goquery.Selection) {
 			text := s.Text()
 			text = strings.Replace(text, "\n", "", -1)
 			text = strings.Replace(text, "\t", "", -1)
 			enc := mahonia.NewEncoder("gbk")
 			str := enc.ConvertString("万")
-			if ( strings.Contains(text, str) ) {
+			if strings.Contains(text, str) {
 				text = strings.Replace(text, str, "", -1)
 				value, _ := strconv.Atoi(text)
 				//value = value * 10000
@@ -32,32 +33,32 @@ func SaveStockFundFlow(db *sql.DB,stockCode string)  {
 				text = strconv.Itoa(value)
 			}
 			str = enc.ConvertString("亿")
-			if ( strings.Contains(text, str) ) {
+			if strings.Contains(text, str) {
 				text = strings.Replace(text, str, "", -1)
 				value, _ := strconv.ParseFloat(text, 64)
 				value = value * 10000
 				text = strconv.FormatFloat(value, 'f', 0, 64)
 			}
-			if ( strings.Contains(text, "%") ) {
+			if strings.Contains(text, "%") {
 				text = strings.Replace(text, "%", "", -1)
 				value, _ := strconv.ParseFloat(text, 64)
 				value = value / 100
 				text = strconv.FormatFloat(value, 'f', 4, 64)
 			}
 			data = append(data, text)
-		});
+		})
 		stmt, err := db.Prepare(`INSERT fund_flow (stockCode,riqi,shoupan,zhangdiefu,
 				zhuli_jinge,zhuli_jingzhanbi,
 				chaodadan_jinge,chaodadan_jingzhanbi,
 				dadan_jinge,dadan_jingzhanbi,
 				zhongdan_jinge,zhongdan_jingzhanbi,
 				xiaodan_jinge,xiaodan_jingzhanbi) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`)
-		if( err!=nil ){
-			fmt.Printf("find exception:%s\r\n",err.Error())
-			return ;
+		if err != nil {
+			fmt.Printf("find exception:%s\r\n", err.Error())
+			return
 		}
 
-		_,err= stmt.Exec("601688",data[1],
+		_, err = stmt.Exec(stockCode, data[1],
 			F(data[2]),
 			F(data[3]),
 			F(data[4]),
@@ -70,9 +71,9 @@ func SaveStockFundFlow(db *sql.DB,stockCode string)  {
 			F(data[11]),
 			F(data[12]),
 			F(data[13]))
-		if( err!=nil ) {
-			fmt.Printf("find exception:%s\r\n",err.Error())
-			return ;
+		if err != nil {
+			fmt.Printf("find exception:%s\r\n", err.Error())
+			return
 		}
-	});
+	})
 }
