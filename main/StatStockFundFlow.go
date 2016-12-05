a85fd4224b5910568b4954fd50ffea33de54c3a7
7481875@qq.com <7481875@qq.com>
2016-12-05 20:13:14 +0800

    增加批处理

--- main/StatStockFundFlow.go
+++ main/StatStockFundFlow.go
@@ -1,9 +1,9 @@
 package main
 
-import _ "github.com/go-sql-driver/mysql"
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"os"
 	"sanjia/finance/stock"
 	"sanjia/finance/util"
